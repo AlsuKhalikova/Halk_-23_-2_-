a43ba7c0ab472a4539846297bba3e25a9c88c035
AlsuKhalikova <149301535+AlsuKhalikova@users.noreply.github.com>
2024-02-21 09:53:50 +0300

    Update task1.py

--- Лаб_3/task1.py
+++ Лаб_3/task1.py
@@ -24,8 +24,25 @@ class PaperBook(Book):
         super().__init__(name, author)
         self.pages = pages
 
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages: int):
+        if not isinstance(new_pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+
+        if new_pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+
+        self._pages = new_pages
+
+    def __str__(self):
+        return f"Бумажная книга {self.name!r}. Автор {self.author}. Количество страниц {self.pages}"
+
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages})"
 
 
 class AudioBook(Book):
@@ -33,5 +50,39 @@ class AudioBook(Book):
         super().__init__(name, author)
         self.duration = duration
 
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration: float):
+        if not isinstance(new_duration, float):
+            raise TypeError("Продолжительность аудиокниги должна быть типа float")
+
+        if new_duration <= 0:
+            raise ValueError("Продолжительность аудиокниги должна быть неотрицательным числом")
+
+        self._duration = new_duration
+
+    def __str__(self):
+        return f"Аудиокнига {self.name!r}. Автор {self.author}. Продолжительность {self.duration}"
+
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration})"
+
+
+book = Book("Гарри Поттер и узник Азкабана", "Дж.К.Роулинг")
+paper_book = PaperBook("Гарри Поттер и узник Азкабана", "Дж.К.Роулинг", 510)
+audio_book = AudioBook("Гарри Поттер и узник Азкабана", "Дж.К.Роулинг", 12.7)
+
+print(book.author)
+print(book.name)
+
+print(paper_book)
+print(repr(paper_book))
+
+print(audio_book.duration)
+
+paper_book.pages = 511
+print(paper_book.pages)
+
